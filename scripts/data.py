ac3c65f9070c2f1cc657a431174208e7134b581f
r3card0 <ricardo.rdz@pm.me>
2022-09-29 22:33:08 -0500

    program description

---
+++ scripts/data.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import os
+from openpyxl.workbook import workbook
+
+def description():
+    program_description = """
+    This program stores dictionaries
+    """
+    return program_description
+
+
+
+def cup_data():
+    cup = {
+
+    }
+
+def run():
+    pass
+
+
+if __name__ == "__main__":
+    run()
