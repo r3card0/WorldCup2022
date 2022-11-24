945a1b6e8de3a71c6658afb586967086efbe0dd8
r3card0 <ricardo.rdz@pm.me>
2022-11-24 15:56:11 -0600

    added insert statements for scores table

--- scripts/inserts.py
+++ scripts/inserts.py
@@ -1,3 +1,8 @@
 def insert_matches():
     insert_ = "INSERT INTO qtar.matches(matchNo,`date`,stadiumId,localTeamId,visitorTeamId,weekNo,phase) VALUES(%s,%s,%s,%s,%s,%s,%s)"
     return insert_
+
+
+def insert_scores():
+    insert_ = "INSERT INTO qtar.scores(matchId,teamId,minute) VALUES(%s,%s,%s)"
+    return insert_
