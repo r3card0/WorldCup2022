3fb89af5e07d8706029a178306309f17aa42ba1f
r3card0 <ricardo.rdz@pm.me>
2022-11-23 21:46:21 -0600

    table matches created

---
+++ db/definition/matches.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS matches
+(
+    matchId INTEGER NOT NULL AUTO_INCREMENT
+    , stadiumId INTEGER NOT NULL
+    , localTeamId INTEGER NOT NULL
+    , visitorTeamId INTEGER NOT NULL
+    , matchNo INTEGER NOT NULL
+    , weekNo INTEGER NOT NULL
+    , phase INTEGER -- ENUM(1,2,3,4)  
+    , `date` DATE
+    , insertDate DATETIME DEFAULT NOW()
+    , CONSTRAINT matches_pk PRIMARY KEY (matchId)
+    -- , CONSTRAINT stadium_fk FOREIGN KEY (stadiumId)
+    -- , CONSTRAINT localTeam_fk FOREIGN KEY (localTeamId)
+    -- , CONSTRAINT visitorTeam_fk FOREIGN KEY (visitorTeamId)
+)
