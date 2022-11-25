0a8dbc9e2c36cc1d702c19e6ef7e1cd065762981
r3card0 <ricardo.rdz@pm.me>
2022-11-25 01:10:55 -0600

    column minute changed by goal

--- db/definition/scores_cr.sql
+++ db/definition/scores_cr.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS scores
     scoreId INTEGER NOT NULL AUTO_INCREMENT
     , matchId INTEGER NOT NULL
     , teamId INTEGER NOT NULL
-    , minute INTEGER NOT NULL
+    , goal INTEGER NOT NULL
     , insertDate DATETIME DEFAULT NOW()
     , CONSTRAINT scores_pk PRIMARY KEY (scoreId)
 )
