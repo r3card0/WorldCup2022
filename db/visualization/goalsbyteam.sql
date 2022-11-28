8304d913a55d0596c7302d01b89dfa10015c838e
r3card0 <ricardo.rdz@pm.me>
2022-11-27 21:49:49 -0600

    added teams schema q

--- db/visualization/goalsbyteam.sql
+++ db/visualization/goalsbyteam.sql
@@ -1,9 +1,9 @@
 SELECT
 t.team_name
-, sum(s.goal)
+, sum(s.goal) `goal`
 FROM qtar.scores s
 JOIN qtar.matches m ON m.matchId = s.matchId
-JOIN soccer.teams t ON t.team_id = s.teamId
+JOIN teams t ON t.team_id = s.teamId
 GROUP BY 1
 ORDER BY 2 desc
 ;
