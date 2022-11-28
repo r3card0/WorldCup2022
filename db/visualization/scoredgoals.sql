9200ac8e7f5be1cd1c60260e9a78c8e4c3da05fd
r3card0 <ricardo.rdz@pm.me>
2022-11-27 22:07:00 -0600

    added goal aveage per day

--- db/visualization/scoredgoals.sql
+++ db/visualization/scoredgoals.sql
@@ -1,4 +1,6 @@
+-- Extraction of total goals per day
 SELECT distinct m.`date`,sum(s.goal) `total`
+, round(avg(s.goal),2) `av_p_day`
 FROM qtar.scores s
 JOIN qtar.matches m ON m.matchId = s.matchId
 GROUP BY 1
