bbedf1d7951c28cfe4b8271b9c1d6a708b6cf0f3
r3card0 <ricardo.rdz@pm.me>
2022-11-27 21:37:19 -0600

    games played by team

---
+++ db/definition/v_gplayed.sql
@@ -0,0 +1,11 @@
+CREATE VIEW qtar.vgplayed AS
+(
+    select g.`group`,
+t.team_name `team`
+,count(s.teamId) `GP` -- games played
+from qtar.scores s
+JOIN soccer.teams t ON t.team_id = s.teamId
+JOIN qtar.`groups`g ON g.teamId = t.team_id
+GROUP BY 1,2
+ORDER BY 1
+);
