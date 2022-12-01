91335c566d5fa952915fa63ee63dbaf970b834cc
r3card0 <ricardo.rdz@pm.me>
2022-12-01 00:08:21 -0600

    added attendances insert statement

--- scripts/inserts.py
+++ scripts/inserts.py
@@ -10,3 +10,7 @@ def insert_scores():
 def insert_players():
     insert_ = "INSERT INTO qtar.players(pname,lastName,teamId,positionId) VALUES(%s,%s,%s,%s)"
     return insert_
+
+def insert_attendances():
+    insert_ = "INSERT INTO qtar.attendances(matchId,attendance) VALUES (%s,%s)"
+    return insert_
