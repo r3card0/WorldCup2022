9ea4cc12408c21932d9037081b0a38c2064ea3a0
r3card0 <ricardo.rdz@pm.me>
2022-12-04 01:17:46 -0600

    schema soccer changed

--- db/definition/v_gplayed.sql
+++ db/definition/v_gplayed.sql
@@ -4,7 +4,7 @@ CREATE VIEW qtar.vgplayed AS
 t.team_name `team`
 ,count(s.teamId) `GP` -- games played
 from qtar.scores s
-JOIN soccer.teams t ON t.team_id = s.teamId
+JOIN qtar.teams t ON t.team_id = s.teamId
 JOIN qtar.`groups`g ON g.teamId = t.team_id
 GROUP BY 1,2
 ORDER BY 1
