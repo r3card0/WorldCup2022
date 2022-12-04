19fc7deb74d559b692eae46a3ab54cadc06a659e
r3card0 <ricardo.rdz@pm.me>
2022-12-04 01:24:25 -0600

    schema soccer changed

--- db/definition/v_goalsbyteam.sql
+++ db/definition/v_goalsbyteam.sql
@@ -5,6 +5,6 @@ t.team_name `team`
 , sum(s.goal) `goal`
 FROM qtar.scores s
 JOIN qtar.matches m ON m.matchId = s.matchId
-JOIN soccer.teams t ON t.team_id = s.teamId
+JOIN qtar.teams t ON t.team_id = s.teamId
 GROUP BY 1
 )
