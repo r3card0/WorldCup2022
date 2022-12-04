48267496b18b6f505055fb0658f75707dfc7f7d2
r3card0 <ricardo.rdz@pm.me>
2022-12-04 10:56:40 -0600

    soccer schema excluded

--- db/visualization/gamesdraw.sql
+++ db/visualization/gamesdraw.sql
@@ -23,7 +23,7 @@ CASE m.phase
     ELSE 'Not considered'
 END phase
 , m.`date`
-, st.stadium_name `stadium`
+, st.stadium
 , lg.`group`lgroup
 , lt.team_name `local_team`
 , ls.goal `lt_goal`
@@ -33,9 +33,9 @@ END phase
 FROM qtar.matches m
 JOIN qtar.scores ls ON m.matchId = ls.matchId
 JOIN qtar.scores vs ON m.matchId = vs.matchId
-JOIN soccer.teams lt ON lt.team_id = m.localTeamId AND lt.team_id = ls.teamId
-JOIN soccer.teams vt ON vt.team_id = m.visitorTeamId AND vt.team_id = vs.teamId
-JOIN soccer.stadiums st ON st.stadium_id = m.stadiumId
+JOIN qtar.teams lt ON lt.team_id = m.localTeamId AND lt.team_id = ls.teamId
+JOIN qtar.teams vt ON vt.team_id = m.visitorTeamId AND vt.team_id = vs.teamId
+JOIN qtar.stadiums st ON st.stadiumId = m.stadiumId
 JOIN qtar.`groups`lg ON lg.teamId = lt.team_id 
 JOIN qtar.`groups`vg ON vg.teamId = vt.team_id 
 ORDER BY 3,1
@@ -89,7 +89,7 @@ CASE m.phase
     ELSE 'Not considered'
 END phase
 , m.`date`
-, st.stadium_name `stadium`
+, st.stadium
 , lg.`group`lgroup
 , lt.team_name `local_team`
 , ls.goal `lt_goal`
@@ -99,9 +99,9 @@ END phase
 FROM qtar.matches m
 JOIN qtar.scores ls ON m.matchId = ls.matchId
 JOIN qtar.scores vs ON m.matchId = vs.matchId
-JOIN soccer.teams lt ON lt.team_id = m.localTeamId AND lt.team_id = ls.teamId
-JOIN soccer.teams vt ON vt.team_id = m.visitorTeamId AND vt.team_id = vs.teamId
-JOIN soccer.stadiums st ON st.stadium_id = m.stadiumId
+JOIN qtar.teams lt ON lt.team_id = m.localTeamId AND lt.team_id = ls.teamId
+JOIN qtar.teams vt ON vt.team_id = m.visitorTeamId AND vt.team_id = vs.teamId
+JOIN qtar.stadiums st ON st.stadiumId = m.stadiumId
 JOIN qtar.`groups`lg ON lg.teamId = lt.team_id 
 JOIN qtar.`groups`vg ON vg.teamId = vt.team_id 
 ORDER BY 3,1
@@ -126,7 +126,7 @@ WHEN lt_goal = vt_goal THEN visitor_team
 END empate2
 FROM base
 ) base 
-JOIN soccer.teams t ON t.team_name = base.empate2
+JOIN qtar.teams t ON t.team_name = base.empate2
 JOIN qtar.`groups`g ON g.teamId = t.team_id 
 WHERE empate2 != ''
 GROUP BY 1,2
