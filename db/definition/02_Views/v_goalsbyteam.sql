d90eaeaca04d7ee2e82e36025b7d676b2184984d
r3card0 <ricardo.rdz@pm.me>
2022-12-08 22:12:50 -0600

    Added phase column

--- db/definition/02_Views/v_goalsbyteam.sql
+++ db/definition/02_Views/v_goalsbyteam.sql
@@ -3,11 +3,13 @@ View to visualize goals by tema
 */
 CREATE VIEW vgoalsbyteam as
 (
-    SELECT
-t.team_name `team`
+SELECT
+t.team_name
 , sum(s.goal) `goal`
+, m.phase
 FROM qtar.scores s
 JOIN qtar.matches m ON m.matchId = s.matchId
-JOIN qtar.teams t ON t.team_id = s.teamId
-GROUP BY 1
+JOIN teams t ON t.team_id = s.teamId
+GROUP BY 1,3
+ORDER BY 2 desc
 )
